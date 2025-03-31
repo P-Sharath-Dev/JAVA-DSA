46f29850399c308475f4c2b2e0ebb5c559b1926f
Sharath <your.email@example.com>
2025-03-31 22:43:24 +0530

    another pattern

--- Patterns1/TrianglePattern.java
+++ Patterns1/TrianglePattern.java
@@ -29,6 +29,18 @@ public class TrianglePattern {
             }
             System.out.println();
         }
+
+        System.out.println("another pattern");
+        // 1
+        // 2 2
+        // 3 3 3
+        // 4 4 4 4
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i ; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
         
         System.out.println("another pattern");
         // 1
